23d9c11c9144ce22e3a29ebb3870df50d3fe006a
kelz <kelsie.langston@yahoo.com>
2020-05-20 19:39:28 -0400

    add proxy back and corrected isLoggedIn function

--- client/src/components/createAccount/createAccount.js
+++ client/src/components/createAccount/createAccount.js
@@ -1,29 +1,30 @@
 import React, { useState, useEffect } from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import axios from "axios";
 
 const CreateAccount = (props) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [displayError, setDisplayError] = useState("");
-  const [isLoggedIn, setIsLoggedIn ] = useState(false);
-  const [flashLogin, setFlashLogin ] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [flashLogin, setFlashLogin] = useState(false);
   const [display, setDisplay] = useState(false);
 
   useEffect(() => {
-    localStorage.getItem('jwtToken') !== "" && setIsLoggedIn(true);
-  }, [])
+    localStorage.getItem("jwtToken") !== "" ||
+      (!localStorage.getItem("jwtToken") && setIsLoggedIn(false));
+  }, []);
 
   const emailValid = (email) => {
     const emailRegex = /^([A-Za-z0-9_\-.+])+@([A-Za-z0-9_\-.])+\.([A-Za-z]{2,})$/;
     return emailRegex.test(email);
-}
+  };
 
   const handleSignup = (e) => {
     e.preventDefault();
 
-    if(!emailValid(email)) {
-      setDisplayError('Invalid email format');
+    if (!emailValid(email)) {
+      setDisplayError("Invalid email format");
       return;
     }
 
@@ -43,67 +44,78 @@ const CreateAccount = (props) => {
 
   const handleLogout = (e) => {
     e.preventDefault();
-    localStorage.setItem('jwtToken', '');
+    localStorage.setItem("jwtToken", "");
     setDisplay(true);
     setIsLoggedIn(false);
-}
+  };
 
   return (
     <>
-    <nav className="navbar navbar-light bg-light">
-        <Link to="/" className="navbar-brand">Spaceship</Link>
-        {isLoggedIn ? <><Link to="/member-profile" className="ml-auto">Profile</Link>
-        <a className="nav-link" href="/" onClick={handleLogout}>Logout</a></>
-        : <Link to="/login">Login</Link>}
-   </nav>
-    <div className="container">
-      <form>
-        {displayError && (
-          <div className="alert alert-danger mt-2" role="alert">
-            {displayError}
-          </div>
+      <nav className="navbar navbar-light bg-light">
+        <Link to="/" className="navbar-brand">
+          Spaceship
+        </Link>
+        {isLoggedIn ? (
+          <>
+            <Link to="/member-profile" className="ml-auto">
+              Profile
+            </Link>
+            <a className="nav-link" href="/" onClick={handleLogout}>
+              Logout
+            </a>
+          </>
+        ) : (
+          <Link to="/login">Login</Link>
         )}
-        {flashLogin && 
-        <div className="alert alert-success mt-2" role="alert">
-            Account Created Successfully!
-        </div>
-        }
-        {display && 
-        <div className="alert alert-success mt-2" role="alert">
-            Logout Successful!
-        </div>
-        }
-        <div className="form-group">
-          <label htmlFor="exampleInputEmail1">Email Address</label>
-          <input
-            type="email"
-            className="form-control"
-            id="exampleInputEmail1"
-            aria-describedby="emailHelp"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="exampleInputPassword1">Password</label>
-          <input
-            type="password"
-            className="form-control"
-            id="exampleInputPassword1"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <button
-          onClick={handleSignup}
-          type="submit"
-          className="btn btn-primary"
-        >
-          Create account
-        </button>
-      </form>
-    </div>
-  </>
+      </nav>
+      <div className="container">
+        <form>
+          {displayError && (
+            <div className="alert alert-danger mt-2" role="alert">
+              {displayError}
+            </div>
+          )}
+          {flashLogin && (
+            <div className="alert alert-success mt-2" role="alert">
+              Account Created Successfully!
+            </div>
+          )}
+          {display && (
+            <div className="alert alert-success mt-2" role="alert">
+              Logout Successful!
+            </div>
+          )}
+          <div className="form-group">
+            <label htmlFor="exampleInputEmail1">Email Address</label>
+            <input
+              type="email"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="exampleInputPassword1">Password</label>
+            <input
+              type="password"
+              className="form-control"
+              id="exampleInputPassword1"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button
+            onClick={handleSignup}
+            type="submit"
+            className="btn btn-primary"
+          >
+            Create account
+          </button>
+        </form>
+      </div>
+    </>
   );
 };
 
--- client/src/components/login/login.js
+++ client/src/components/login/login.js
@@ -1,23 +1,24 @@
 import React, { useState, useEffect } from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import axios from "axios";
 
 const SignIn = (props) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [displayError, setDisplayError] = useState("");
-  const [isLoggedIn, setIsLoggedIn ] = useState(false);
-  const [flashLogin, setFlashLogin ] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [flashLogin, setFlashLogin] = useState(false);
   const [display, setDisplay] = useState(false);
 
   useEffect(() => {
-    localStorage.getItem('jwtToken') !== "" && setIsLoggedIn(true);
-  }, [])
+    localStorage.getItem("jwtToken") !== "" ||
+      (!localStorage.getItem("jwtToken") && setIsLoggedIn(false));
+  }, []);
 
   const handleSignin = (e) => {
     e.preventDefault();
     setDisplayError("");
-    
+
     axios
       .post("/api/login", { email, password })
       .then((data) => {
@@ -34,69 +35,80 @@ const SignIn = (props) => {
 
   const handleLogout = (e) => {
     e.preventDefault();
-    localStorage.setItem('jwtToken', '');
+    localStorage.setItem("jwtToken", "");
     setDisplay(true);
     setTimeout(() => {
-       props.history.push('/');
-    }, 1200)
-}
+      props.history.push("/");
+    }, 1200);
+  };
 
   return (
     <>
-    <nav className="navbar navbar-light bg-light">
-        <Link to="/" className="navbar-brand">Spaceship</Link>
-        {isLoggedIn ? <><Link to="/member-profile" className="ml-auto">Profile</Link>
-        <a className="nav-link" href="/" onClick={handleLogout}>Logout</a></>
-        : <Link to='/create-account'>Create Account</Link>}
-    </nav>
-    <div className="container">
+      <nav className="navbar navbar-light bg-light">
+        <Link to="/" className="navbar-brand">
+          Spaceship
+        </Link>
+        {isLoggedIn ? (
+          <>
+            <Link to="/member-profile" className="ml-auto">
+              Profile
+            </Link>
+            <a className="nav-link" href="/" onClick={handleLogout}>
+              Logout
+            </a>
+          </>
+        ) : (
+          <Link to="/create-account">Create Account</Link>
+        )}
+      </nav>
+      <div className="container">
         {displayError && (
           <div className="alert alert-danger mt-2" role="alert">
             {displayError}
           </div>
         )}
-        {flashLogin && 
-        <div className="alert alert-success mt-2" role="alert">
+        {flashLogin && (
+          <div className="alert alert-success mt-2" role="alert">
             Login Successful!
-        </div>
-        }
-        {display && 
-        <div className="alert alert-success mt-2" role="alert">
+          </div>
+        )}
+        {display && (
+          <div className="alert alert-success mt-2" role="alert">
             Logout Successful!
-        </div>
-        }
-      <form>
-        <div className="form-group">
-          <label htmlFor="exampleInputEmail1">Email Address</label>
-          <input
-            type="email"
-            className="form-control"
-            id="exampleInputEmail1"
-            aria-describedby="emailHelp"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="exampleInputPassword1">Password</label>
-          <input
-            type="password"
-            className="form-control"
-            id="exampleInputPassword1"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <button
-          onClick={handleSignin}
-          type="submit"
-          className="btn btn-primary"
-        >
-          Sign in
-        </button>
-      </form>
-    </div>
-  </>
+          </div>
+        )}
+        <form>
+          <div className="form-group">
+            <label htmlFor="exampleInputEmail1">Email Address</label>
+            <input
+              type="email"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="exampleInputPassword1">Password</label>
+            <input
+              type="password"
+              className="form-control"
+              id="exampleInputPassword1"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button
+            onClick={handleSignin}
+            type="submit"
+            className="btn btn-primary"
+          >
+            Sign in
+          </button>
+        </form>
+      </div>
+    </>
   );
 };
 
